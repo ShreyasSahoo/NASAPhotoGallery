185aeaa9bbb3e1b4ea3ae743c10dc4bc119fbd15
Shreyas Sahoo <shreyassahoo26@gmail.com>
2025-12-15 12:50:43 +0530

    [Feature] Add colorsets & theme

---
+++ NASAPhotoGallery/Utilities/Theme.swift
@@ -0,0 +1,21 @@
+//
+//  Theme.swift
+//  NASAPhotoGallery
+//
+//  Created by Shreyas on 15/12/25.
+//
+
+import SwiftUI
+
+struct Theme {
+
+    static let backgroundPrimary = Color("BackgroundPrimary")
+    static let backgroundSecondary = Color("BackgroundSecondary")
+    static let cardBackground = Color("CardBackground")
+
+    static let primaryText = Color("PrimaryText")
+    static let secondaryText = Color("SecondaryText")
+
+    static let accent = Color("Accent")
+    static let favourite = Color("Favourite")
+}
